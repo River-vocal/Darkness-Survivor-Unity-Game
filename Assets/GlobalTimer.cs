3505b593b846270321f1ce5d928dca431b21d507
joeymoso <94441074+joeymoso@users.noreply.github.com>
2022-09-11 14:31:34 -0700

    Add grid movement

---
+++ Assets/GlobalTimer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalTimer : MonoBehaviour
+{
+    public static int millSec;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        millSec = (int) ((Time.fixedTime * 1000) % 1000);
+    }
+}
