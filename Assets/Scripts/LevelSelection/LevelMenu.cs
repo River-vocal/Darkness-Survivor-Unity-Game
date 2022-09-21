1016ba23d8d7782a4c22ee11250df486ea2d2daa
Tianli Tong <tianlito@usc.edu>
2022-09-21 16:32:57 -0700

    Add game menu(no art)

---
+++ Assets/Scripts/LevelSelection/LevelMenu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelMenu : MonoBehaviour
+{
+    public void StartScene(int levelIndex)
+    {
+        SceneManager.LoadScene(levelIndex);
+    }
+
+    public void Back()
+    {
+
+    }
+}
