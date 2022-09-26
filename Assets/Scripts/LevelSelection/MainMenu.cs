bf911ae0726d12447e680f540527201919029006
jackStudyCode <lzx493131129@gmail.com>
2022-09-25 22:27:51 -0700

    Add a better tutorial Scene for Scene change.

--- Assets/Scripts/LevelSelection/MainMenu.cs
+++ Assets/Scripts/LevelSelection/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(3);
     }
 
     public void QuitGame()
