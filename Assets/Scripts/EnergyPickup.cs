f80f00ea69ecdf21a69f691a5b04fca1f0856a68
Tianli Tong <tianlito@usc.edu>
2022-10-13 17:55:36 -0700

    Added energy boost

---
+++ Assets/Scripts/EnergyPickup.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyPickup : MonoBehaviour
+{
+    private float boostValue = 10f;
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.tag == "Player"){
+            Energy energy = other.gameObject.GetComponent<Energy>();
+            energy.CurEnergy+=boostValue;
+            Destroy(gameObject);
+        }
+    }
+}
