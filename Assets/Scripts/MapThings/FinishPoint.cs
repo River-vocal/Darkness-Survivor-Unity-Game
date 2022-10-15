8d2506c4f3db80f86b3aa200d3fe54a80676be50
EddyHonkMan <rui.zhu119@gmail.com>
2022-10-14 17:05:18 -0700

    Added tutorial scene

---
+++ Assets/Scripts/MapThings/FinishPoint.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishPoint : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("Play reach finish point");
+        // TODO: Load next scene
+    }
+}
