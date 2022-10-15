60c156f7f491183a924c3d7ae9580309775376ea
EddyHonkMan <rui.zhu119@gmail.com>
2022-10-14 17:20:21 -0700

    Added exit

--- Assets/Scripts/MapThings/Exit.cs
+++ Assets/Scripts/MapThings/Exit.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FinishPoint : MonoBehaviour
+public class Exit : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D col)
     {
