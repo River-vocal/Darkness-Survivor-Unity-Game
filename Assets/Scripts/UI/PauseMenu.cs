abaed90b173f46ed8eeecfd2666008fa50057085
jackStudyCode <lzx493131129@gmail.com>
2022-10-16 13:00:20 -0700

    Adjust boss move speed and add allPassMenu

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -14,7 +14,7 @@ public class PauseMenu : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.P))
         {
             if (GameIsPaused)
             {
