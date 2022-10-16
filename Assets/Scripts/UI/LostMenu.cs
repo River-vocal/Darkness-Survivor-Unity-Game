be077211a1aef4f27010653ade51d115a7c37df0
Tianli Tong <tianlito@usc.edu>
2022-10-16 12:01:20 -0700

    Timescale in LostMenu.Awake()

--- Assets/Scripts/UI/LostMenu.cs
+++ Assets/Scripts/UI/LostMenu.cs
@@ -9,11 +9,11 @@ public class LostMenu : MonoBehaviour
     private Energy energy;
     private GameObject lostMenu;
 
-    private void Start()
-    {
+    private void Awake() {
         energy = GameObject.FindWithTag("Player").GetComponent<Energy>();
         energy.OnEmpty += energy_OnEmpty;
         lostMenu = transform.Find("lostMenu").gameObject;
+        Time.timeScale = 1f;
     }
 
     private void energy_OnEmpty(object sender, System.EventArgs e)
