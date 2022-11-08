26bb1c2310d230aaeea1d125df1bd0605bda3518
Tianli Tong <tianlito@usc.edu>
2022-11-07 23:14:27 -0800

    Add Time.delaTime to fix speed difference

--- Assets/Scripts/MapThings/Pickup/ElfBlessing.cs
+++ Assets/Scripts/MapThings/Pickup/ElfBlessing.cs
@@ -52,7 +52,8 @@ public class ElfBlessing : MonoBehaviour
         if (!collided || !target) return;
         angle += Time.deltaTime * rotationSpeed;
 
-        transform.position = Vector3.MoveTowards(transform.position, target.position + TargetOffset, smoothTime);
+        transform.position = Vector3.MoveTowards(transform.position, target.position + TargetOffset,
+            smoothTime * Time.deltaTime);
     }
 
     private IEnumerator EndBlessing()
