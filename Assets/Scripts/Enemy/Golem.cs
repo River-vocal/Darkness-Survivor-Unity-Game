52b96623c98af9933e48bcf3696ebbb5e5f447e7
YizhuoMeng <yizhuome@usc.edu>
2022-11-14 18:18:52 -0800

    Add chasing projectiles to golem

--- Assets/Scripts/Enemy/Golem.cs
+++ Assets/Scripts/Enemy/Golem.cs
@@ -8,69 +8,51 @@ public class Golem : MonoBehaviour
     [SerializeField] public int damage;
     [SerializeField] public float speed;
     private float distance = 2f;
-    // [SerializeField] public int lives_num;
     private bool movingRight = true;
     public Transform groundDetection;
-    // [SerializeField] GameObject projectile;
+    public GameObject projectile;
+    private float timeBtwShots;
+    public float startTimeBtwShots;
     private Animator golem_animation;
-    private float width;
-    private float height;
-    private SpriteRenderer sp;
-    // float fireRate;
-    // float nextFire;
 
     private void Awake()
     {
         golem_animation = GetComponent<Animator>();
-        width = GetComponent<Collider2D>().bounds.size.x;
-        height = GetComponent<Collider2D>().bounds.size.y;
-        sp = GetComponent<SpriteRenderer>();
+
+        
     }
-    // Start is called before the first frame update
-    // void Start()
-    // {
-    //     fireRate = 1f;
-    //     nextFire = Time.time;   
-    // }
 
     // Update is called once per frame
     void Update()
     {
-        // CheckIfTimeToFire();
         transform.Translate(Vector2.right * speed * Time.deltaTime);
         int layer_mask = LayerMask.GetMask ("Ground");
         RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance, layer_mask);
-        // bool groundInfo = Physics2D.OverlapCircle(groundDetection.position, distance, layer_mask);
         if(groundInfo.collider == false)
         {
             if(movingRight == true)
             {
                 transform.eulerAngles = new Vector3(0, -180, 0);
-                Debug.Log("Turn Left!!!!!!!!!!!!!!");
                 movingRight = false;
             }
             else
             {
                 transform.eulerAngles = new Vector3(0, 0, 0);
-                Debug.Log("Turn right!!!!!!!!!!!!!!");
                 movingRight = true;
                 
             }
         }
-
-
-        
-        
+        if(timeBtwShots <= 0)
+        {
+        Instantiate(projectile, transform.position, Quaternion.identity);
+        timeBtwShots = startTimeBtwShots;
+        }
+        else
+        {
+        timeBtwShots -= Time.deltaTime;
+        }               
     }
 
-    // void CheckIfTimeToFire()
-    // {
-    //     if (Time.time > nextFire)
-    //     {
-    //         Instantiate (projectile, transform.position, Quaternion.identity);
-    //         nextFire = Time.time + fireRate;
-    //     }
-    // }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -86,11 +68,6 @@ public class Golem : MonoBehaviour
             }
             GlobalAnalysis.smallenemy_damage += damage;
             other.GetComponent<Energy>().CurEnergy -= damage;
-        	// if (player.transform.position.x >= transform.position.x)
-            // {
-            //     Flip();
-	    	// 	Debug.Log("Flipped!!!!!!!!!!!!!!!!!");
-	    	// }
         }
     }
 
@@ -111,14 +88,5 @@ public class Golem : MonoBehaviour
     private void Deactivate()
     {
         gameObject.SetActive(false);
-    }
-
-    
-
-    // void Flip()
-    // {
-    //     transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
-    // }
-
-    
+    }    
 }
--- Assets/Scripts/Enemy/GolemProjectile.cs
+++ Assets/Scripts/Enemy/GolemProjectile.cs
@@ -4,42 +4,65 @@ using UnityEngine;
 
 public class GolemProjectile : MonoBehaviour
 {
-    [SerializeField] public float moveSpeed;
-    Rigidbody2D rb;
-    Player target;
-    private Transform myTransform;
-    Vector2 moveDirection;
+    public float moveSpeed;
+    public int damage;
+    private Transform player;
+    private Rigidbody2D playerDirection;
+    private Vector2 target;
+    private Vector2 originalPosition;
+    public float shootingDistance;
     
-
-    void Awake()
-    {
-        rb.GetComponent<Rigidbody2D>();
-        target = GameObject.FindObjectOfType<Player>();
-        moveDirection = (target.transform.position - transform.position).normalized * moveSpeed;
-        rb.velocity = new Vector2 (moveDirection.x, moveDirection.y);
-        Destroy(gameObject, 3f);
-    }
     // Start is called before the first frame update
     void Start()
     {
-        
-        
+        player = GameObject.FindWithTag("Player").transform;
+        playerDirection = GameObject.FindWithTag("Player").GetComponent<Rigidbody2D>();
+        originalPosition = new Vector2(transform.position.x, transform.position.y);
+        target = new Vector2(player.position.x, player.position.y);
     }
 
     // Update is called once per frame
     void Update()
     {
+        transform.position = Vector2.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
         
-        
+        FlipProjectile();
+
+        if(transform.position.x == player.position.x && transform.position.y == player.position.y)
+        {
+            Destroy(gameObject);
+        }
+
+        if(Vector2.Distance(transform.position, originalPosition) > shootingDistance)
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // void OnTriggerEnter2D(Collider2D other)
-    // {
-    //     if(other.gameObject.CompareTag("Player"))
-    //     {
-    //         Debug.Log("Hit projectile!!!!!!!!!!!!!!!!!!!!!!");
-    //         Destroy(gameObject);
-    //     }
-        
-    // }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+            if (other.GetComponent<Energy>().CurEnergy < damage) {
+                GlobalAnalysis.player_status = "smallenemy_dead";
+                Debug.Log("lose by: small enemy");
+            }
+            GlobalAnalysis.smallenemy_damage += damage;
+            other.GetComponent<Energy>().CurEnergy -= damage;
+        }
+    }
+
+    void FlipProjectile()
+    {
+        if(player.position.x > gameObject.transform.position.x)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+        if(player.position.x < gameObject.transform.position.x)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+    }
+
 }
