13c11c5738362fcc07b827834c0418ce06d720e0
Tianli Tong <tianlito@usc.edu>
2022-11-19 18:34:54 -0800

    Add Speed Pickup and tested on TestScene

---
+++ Assets/prefabs/Characters/Player/Scripts/SpeedChanger.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedChanger : MonoBehaviour
+{
+    [SerializeField] public PlayerData playerData;
+    [SerializeField] public GameObject speedEffectParticleSystem;
+    private float timer;
+    private float originMoveSpeed;
+    private float originDashSpeed;
+
+    private void Awake()
+    {
+        originMoveSpeed = playerData.movementVelocity;
+        originDashSpeed = playerData.dashVelocity;
+    }
+
+    public void SpeedUp(float factor, float time)
+    {
+        timer += time;
+        playerData.movementVelocity = originMoveSpeed * factor;
+        playerData.dashVelocity = originMoveSpeed * factor;
+        speedEffectParticleSystem.SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (timer <= 0) return;
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            playerData.movementVelocity = originMoveSpeed;
+            playerData.dashVelocity = originDashSpeed;
+            speedEffectParticleSystem.SetActive(false);
+        }
+    }
+
+    private void OnDisable()
+    {
+        playerData.movementVelocity = originMoveSpeed;
+        playerData.dashVelocity = originDashSpeed;
+    }
+}
