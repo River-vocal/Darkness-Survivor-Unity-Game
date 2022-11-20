2a61e7b6e5ce0c846e563d48744ed360739b87d0
EddyHonkMan <rui.zhu119@gmail.com>
2022-11-19 23:42:13 -0800

    change name

--- Assets/Scripts/VisualEffectSystem/VisualEffectSystemManager.cs
+++ Assets/Scripts/VisualEffectSystem/VisualEffectSystemManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ParticleSystemManager : MonoBehaviour
+public class VisualEffectSystemManager : MonoBehaviour
 {
     public GameObject BleedParticleEffect;
     public GameObject playerBulletPrefab;
