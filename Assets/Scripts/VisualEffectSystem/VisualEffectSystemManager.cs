f21c6fdc46e7032549f0c890be515ad86b419ddc
EddyHonkMan <rui.zhu119@gmail.com>
2022-11-20 16:42:00 -0800

    change hit boss effect

--- Assets/Scripts/VisualEffectSystem/VisualEffectSystemManager.cs
+++ Assets/Scripts/VisualEffectSystem/VisualEffectSystemManager.cs
@@ -6,6 +6,7 @@ public class VisualEffectSystemManager : MonoBehaviour
 {
     public GameObject BleedParticleEffect;
     public GameObject playerBulletPrefab;
+    public GameObject HitSparklingEffect;
     
     // Start is called before the first frame update
     void Start()
@@ -20,5 +21,10 @@ public class VisualEffectSystemManager : MonoBehaviour
     public void GenerateBleedParticleEffect(Transform transform)
     {
         Instantiate(BleedParticleEffect, transform.position, transform.rotation);
+    }
+
+    public void GenerateHitSparklingEffect(Transform transform)
+    {
+        Instantiate(HitSparklingEffect, transform.position, transform.rotation);
     }
 }
--- Assets/prefabs/Characters/Player/Scripts/PlayerStates/Substates/PlayerAttackState.cs
+++ Assets/prefabs/Characters/Player/Scripts/PlayerStates/Substates/PlayerAttackState.cs
@@ -46,7 +46,8 @@ public class PlayerAttackState : PlayerUseAbilityState
                 //particle effects
                 if (collision.GetComponent<Boss>() != null)
                 {
-                    Player.VisualEffectSystemManager.GenerateBleedParticleEffect(collision.GetComponent<Boss>().transform);
+                    // Player.VisualEffectSystemManager.GenerateBleedParticleEffect(collision.GetComponent<Boss>().transform);
+                    Player.VisualEffectSystemManager.GenerateHitSparklingEffect(collision.GetComponent<Boss>().transform);
                 }
                 Health health = collision.GetComponent<Health>();
                 if (health) health.CurHealth -= PlayerData.attackDamage;
