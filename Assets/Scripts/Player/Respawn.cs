ce600d7c40bfd714fafced399b7af53f8ca54674
Tianli Tong <tianlito@usc.edu>
2022-11-23 20:45:59 -0800

    Add mercy mechanism: Player will get a long lasting elf blessing when he dies too many times.

--- Assets/Scripts/Player/Respawn.cs
+++ Assets/Scripts/Player/Respawn.cs
@@ -1,20 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Respawn : MonoBehaviour
 {
     public Vector3 respawnLocation;
+    public int deathTooManyThreshold = 10;
+
+    private int numOfDeath;
+
     // Start is called before the first frame update
     void Start()
-    {   
+    {
         respawnLocation = transform.transform.position;
+        numOfDeath = 0;
     }
 
     public void Remake()
     {
         resetPlayer();
         gameObject.transform.position = respawnLocation;
+        numOfDeath++;
+        if (numOfDeath >= deathTooManyThreshold)
+        {
+            Instantiate(GameAssets.i.pfElfBlessing, transform.position + new Vector3(0, 3, 0), Quaternion.identity);
+        }
     }
 
     private void resetPlayer()
@@ -23,8 +34,7 @@ public class Respawn : MonoBehaviour
         Player player = GetComponent<Player>();
         player.StateMachine.ChangeState(player.IdleState);
         energy.CurEnergy = energy.MaxEnergy;
-        
+
         // reset more variables here
     }
-    
 }
