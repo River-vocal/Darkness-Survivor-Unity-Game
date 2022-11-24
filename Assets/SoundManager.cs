4fed7b49e05b89c88d8fb0af603765525347be88
EddyHonkMan <rui.zhu119@gmail.com>
2022-11-23 17:20:35 -0800

    add bgm

--- Assets/SoundManager.cs
+++ Assets/SoundManager.cs
@@ -1,17 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    public AudioClip move, jump, land, dash, attack, injured, background, boss;
+    public AudioClip move, jump, land, dash, attack, injured, boss;
 
     private AudioSource audioSource;
     
     // Start is called before the first frame update
     void Start()
     {
-        audioSource = GetComponent<AudioSource>();
+        audioSource = gameObject.AddComponent<AudioSource>();
     }
 
     public void PlaySound(string name)
@@ -19,29 +20,40 @@ public class SoundManager : MonoBehaviour
         switch (name)
         {
             case "move":
+            {
                 audioSource.PlayOneShot(move);
                 break;
+            }
             case "jump":
+            {
                 audioSource.PlayOneShot(jump);
                 break;
+            }
             case "land":
+            {
                 audioSource.PlayOneShot(land);
                 break;
+            }
             case "dash":
+            {
                 audioSource.PlayOneShot(dash);
                 break;
+            }
             case "attack":
+            {
                 audioSource.PlayOneShot(attack);
                 break;
+            }
             case "injured":
+            {
                 audioSource.PlayOneShot(injured);
                 break;
-            case "background":
-                audioSource.PlayOneShot(background);
-                break;
+            }
             case "boss":
+            {
                 audioSource.PlayOneShot(boss);
                 break;
+            }
         }
     }
 }
