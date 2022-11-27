d9ed2cd51ef40ca9e63e920ea995d428226110ae
YizhuoMeng <yizhuome@usc.edu>
2022-11-26 21:49:30 -0800

    Upgrade Piranha and its bullets

--- Assets/Scripts/Enemy/LittleEnemy/Piranha.cs
+++ Assets/Scripts/Enemy/LittleEnemy/Piranha.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public class Piranha : MonoBehaviour
 {
-    private LittleEnemy littleEnemy;
+    [SerializeField] private LittleEnemy littleEnemy;
     public GameObject PiranhaBullet;
-    public float startTimeBtwShots;
-    public float StartShootingDistance;
-    private float timeBtwShots;
+    // public float startTimeBtwShots;
+    // public float StartShootingDistance;
+    // private float timeBtwShots;
     private float Distance2Player;
     private Transform player_transform;
     private bool Piranha_status = true;
+    private Vector2 OriginalPosition;
+    private float height;
+    public float moveSpeed;
+    public Transform groundDetectionDown;
+    public Transform groundDetectionUp;
+    public UnityEngine.Rendering.Universal.Light2D Light2D;
+    private float ground_distance = 0.01f;
+    public float appearDistance;
+    public float godownDistance;
+    private Animator animator;
     // private Golem golem;
 
 
@@ -21,7 +31,10 @@ public class Piranha : MonoBehaviour
     private void Awake()
     {
         player_transform = GameObject.FindWithTag("Player").transform;
-        timeBtwShots = startTimeBtwShots;
+        // timeBtwShots = startTimeBtwShots;
+        OriginalPosition = new Vector2(transform.position.x, transform.position.y);
+        height = GetComponent<SpriteRenderer>().bounds.size.y;
+        
         // Collider2D = GetComponent<Collider2D>();
         // damage = littleEnemy.damage;
     }
@@ -41,10 +54,51 @@ public class Piranha : MonoBehaviour
         // {
         // timeBtwShots -= Time.deltaTime;
         // }
+        if(Vector2.Distance(player_transform.position, transform.position) < appearDistance && Vector2.Distance(player_transform.position, transform.position) > godownDistance)
+        {
+            Appear();
+        }
+        if(Vector2.Distance(player_transform.position, transform.position) > appearDistance || Vector2.Distance(player_transform.position, transform.position) < godownDistance)
+        {
+            Godown();
+        }
+        if( player_transform.position.x > transform.position.x )
+        {
+            Godown();
+        }
+        
+        
+         
+    }
+
+    private void Appear()
+    {
+        int layer_mask_ground = LayerMask.GetMask("Ground");
+        RaycastHit2D groundInfo_down = Physics2D.Raycast(groundDetectionDown.position, Vector2.down, ground_distance, layer_mask_ground);
+        if(groundInfo_down.collider == true)
+        {
+            transform.position += transform.up * Time.deltaTime * moveSpeed;
+        }
+        Light2D.enabled = true;
+    }
+
+    private void Godown()
+    {
+        int layer_mask_ground = LayerMask.GetMask("Ground");
+        RaycastHit2D groundInfo_up = Physics2D.Raycast(groundDetectionUp.position, Vector2.up, ground_distance, layer_mask_ground);
+        if(groundInfo_up.collider == false)
+        {
+            transform.position += - transform.up * Time.deltaTime * moveSpeed;
+        }
+        Light2D.enabled = false;
     }
 
     private void CreateBullets()
     {
-        Instantiate(PiranhaBullet, transform.position, Quaternion.identity);
+        if(player_transform.position.x < gameObject.transform.position.x)
+        {
+            Instantiate(PiranhaBullet, transform.position, Quaternion.identity);
+        }
+        
     }
 }
--- Assets/Scripts/Enemy/LittleEnemy/PiranhaBullet.cs
+++ Assets/Scripts/Enemy/LittleEnemy/PiranhaBullet.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PiranhaBullet : MonoBehaviour
 {
-    private LittleEnemy littleEnemy;
+    [SerializeField] private LittleEnemy littleEnemy;
     public float moveSpeed;
     public int damage;
     private Transform player;
@@ -27,11 +27,13 @@ public class PiranhaBullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // if (littleEnemy.GetBeAttackedStatus())
-        // {
-        //     Destroy(gameObject);
-        //     return;
-        // }
+        if (littleEnemy.GetBeAttackedStatus())
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+
         if(transform.position.x == target.x && transform.position.y == target.y)
         {
             Destroy(gameObject);
