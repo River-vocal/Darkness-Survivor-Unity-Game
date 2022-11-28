ca6ec5872a592cad25d279e950db9e1c15ad59a4
EddyHonkMan <rui.zhu119@gmail.com>
2022-11-28 02:06:54 -0800

    now when boss defeated, no more hint

--- Assets/Scripts/MapThings/MessageZone.cs
+++ Assets/Scripts/MapThings/MessageZone.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 
 public class MessageZone : MonoBehaviour
 {
+    public GameObject boss = null;
     [TextArea] [SerializeField] public String message = "Empty Message";
     [SerializeField] public float displayMinimumTime = 3f;
 
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (!col.gameObject.CompareTag("Player")) return;
-        HintArea.Hint(message, displayMinimumTime);
+        if (boss != null)
+        {
+            if (boss.activeSelf)
+            {
+                HintArea.Hint(message, displayMinimumTime);
+            }
+        }
+        else
+        {
+            HintArea.Hint(message, displayMinimumTime);
+        }
     }
 }
